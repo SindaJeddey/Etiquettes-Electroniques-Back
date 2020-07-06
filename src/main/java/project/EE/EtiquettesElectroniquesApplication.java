46113dae6fcd83c6bc8605b01fb6a296a13bb0bc
SindaJeddey <sindajeddey@gmail.com>
2020-07-06 12:57:31 +0200

    First commit

---
+++ src/main/java/project/EE/EtiquettesElectroniquesApplication.java
@@ -0,0 +1,13 @@
+package project.EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtiquettesElectroniquesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtiquettesElectroniquesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/EE/EtiquettesElectroniquesApplicationTests.java
@@ -0,0 +1,13 @@
+package project.EE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtiquettesElectroniquesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

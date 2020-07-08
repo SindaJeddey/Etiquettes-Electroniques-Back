95e8806e2c90e39a44381a626ebd24996c7498f4
SindaJeddey <sindajeddey@gmail.com>
2020-07-08 10:33:37 +0200

    Implementing security config class.

--- src/main/java/project/EE/security/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/project/EE/security/configuration/ApplicationSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package project.EE.security.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+    }
 }

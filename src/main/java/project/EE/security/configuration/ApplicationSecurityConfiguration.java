fa56b249e4ea6940609d2adf7c70b96395ac6f71
SindaJeddey <sindajeddey@gmail.com>
2020-07-14 09:54:45 +0200

    Adding configurations to the config class

--- src/main/java/project/EE/security/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/project/EE/security/configuration/ApplicationSecurityConfiguration.java
@@ -39,6 +39,10 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure()
+                .and()
                 .cors().and()
                 .csrf().disable()
                 .sessionManagement()
